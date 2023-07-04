19eb0552eb519fa3cd09ddfc79493873a70b039d
Felippe Nagy <flppprojects@outlook.com>
2023-07-03 21:04:53 -0400

    fix: :wrench: fixed add_car request

--- userAdm/userAdm/urls.py
+++ userAdm/userAdm/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from django.contrib import admin
-from car.views import hello2_world, insert_driver
+from car.views import hello2_world, insert_driver, add_car
 from blog.views import hello1_world, post_list, post_create
 from questions.views import hello3_world, create_question
 from userMain.views import hello_world, create_user
@@ -18,5 +18,6 @@ urlpatterns = [
     path('blog/post/', post_create, name='post_create'),
     path("cars/", include("car.urls")),
     path("cars/create/", insert_driver, name='insert_driver'),
+    path("cars/create/car", add_car, name="add_car"),
     path("question/create/", create_question, name='create_question'),
 ]
